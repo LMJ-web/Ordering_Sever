df768dcdfceb62f0e4f60f42949eff3b910f9d01
LMJ <11382765+lin-tianhai@user.noreply.gitee.com>
2024-07-05 18:13:51 +0800

    补充项目截图

---
+++ _sever/Controllers/WXMiniProgram/WX_OrderController.cs
@@ -0,0 +1,35 @@
+﻿using _sever.EF_Core.OrderAndDetail;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace _sever.Controllers.WXMiniProgram
+{
+    [Controller]
+    [Route("[controller]/[action]")]
+    public class WX_OrderController : ControllerBase
+    {
+        private readonly OrderDbContext orderDbContext;
+        private readonly IDistributedCache redis;
+        public WX_OrderController(OrderDbContext orderDbContext, IDistributedCache redis)
+        {
+            this.orderDbContext = orderDbContext;
+            this.redis = redis;
+        }
+        [HttpGet]
+        public IActionResult GetOrderHistory(string session_key)
+        {
+            Console.WriteLine(session_key);
+            string openid = redis.GetString(session_key);
+            if (openid == null) { return BadRequest("登录过期，请先登录"); }
+            Order[] orders = orderDbContext.Orders.Where(order => order.OpenId == openid).OrderByDescending(order=>order.DateTime).ToArray();
+            if (orders.Length == 0) { return BadRequest("无数据"); }
+            return Ok(orders);
+        }
+        [HttpGet]
+        public IActionResult GetOrderHistoryDetailById(string orderId) {
+            Guid _orderId = new Guid(orderId);
+            OrderDetail[] orderDetails = orderDbContext.OrderDetails.Where(orderDetail=>orderDetail.OrderId == _orderId).ToArray();
+            return Ok(orderDetails);
+        }
+    }
+}
