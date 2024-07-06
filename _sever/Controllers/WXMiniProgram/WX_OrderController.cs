2315c224e275b0180064e2fb6d83c66aaa0dd283
LMJ <11382765+lin-tianhai@user.noreply.gitee.com>
2024-07-06 21:50:24 +0800

    更行截图

--- _sever/Controllers/WXMiniProgram/WX_OrderController.cs
+++ _sever/Controllers/WXMiniProgram/WX_OrderController.cs
@@ -20,13 +20,14 @@ namespace _sever.Controllers.WXMiniProgram
         {
             Console.WriteLine(session_key);
             string openid = redis.GetString(session_key);
-            if (openid == null) { return BadRequest("登录过期，请先登录"); }
+            if (openid == null || session_key == null) { return BadRequest("登录过期，请先登录"); }
             Order[] orders = orderDbContext.Orders.Where(order => order.OpenId == openid).OrderByDescending(order=>order.DateTime).ToArray();
             if (orders.Length == 0) { return BadRequest("无数据"); }
             return Ok(orders);
         }
         [HttpGet]
         public IActionResult GetOrderHistoryDetailById(string orderId) {
+            if (orderId == null) { return BadRequest("无效订单Id"); }
             Guid _orderId = new Guid(orderId);
             OrderDetail[] orderDetails = orderDbContext.OrderDetails.Where(orderDetail=>orderDetail.OrderId == _orderId).ToArray();
             return Ok(orderDetails);
